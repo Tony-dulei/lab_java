f686c6f1f4c5fd4f826765eafc2447bfee41366b
Lei Du <lei.du@mail.rakuten.com>
2016-08-30 12:51:41 +0900

    added questions

---
+++ src/api/java/lab/ArrayLab.java
@@ -0,0 +1,20 @@
+package lab;
+
+public class ArrayLab {
+
+// In an array 1-100 numbers are stored, one number is missing how do you find it? 
+// In an array 1-100 exactly one number is duplicate how do you find it?
+// In an array 1-100 multiple numbers are duplicates, how do you find it?
+// Given two arrays, 1,2,3,4,5 and 2,3,1,0,5 find which number is not present in the second array.
+// How do you find second highest number in an integer array?
+// How to find all pairs in array of integers whose sum is equal to given number?
+// How to remove duplicate elements from array in Java?
+// How to find largest and smallest number in array? 
+// How to find top two maximum numbers in array? 
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/api/java/lab/StringLab.java
+++ src/api/java/lab/StringLab.java
@@ -2,6 +2,23 @@ package lab;
 
 public class StringLab {
 
+//	1. Write code to check a String is palindrome or not?
+
+// 2. Write a method which will remove any given character from a String?
+
+// 3. Print all permutation of String both iterative and Recursive way?
+
+// 4. Write a function to find out longest palindrome in a given string?
+
+// 5. How to find first non-repeated character of a given String?
+
+// 6. How to count occurrence of a given character in a String?
+
+// 7. How to check if two String are Anagram?
+
+// 8. How to convert numeric String to int in
+
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
