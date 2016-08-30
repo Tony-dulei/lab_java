01cacc4c7b99148fce45102e2821a717e504e660
Lei Du <lei.du@mail.rakuten.com>
2016-08-30 13:02:02 +0900

    linkedlist and arraylist

--- src/api/java/lab/ArrayLab.java
+++ src/api/java/lab/ArrayLab.java
@@ -1,5 +1,9 @@
 package lab;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class ArrayLab {
 
 // In an array 1-100 numbers are stored, one number is missing how do you find it? 
@@ -14,7 +18,39 @@ public class ArrayLab {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+        // Array Example
+        String[] arrayobj = new String[3];
+        arrayobj[0]= "Love yourself";
+        arrayobj[1]= "Alive is awesome";
+        arrayobj[2]= "Be in Present";
+        System.out.println("====== Array object output : ======");
+        for(int i=0; i < arrayobj.length ;i++) {
+            System.out.println(arrayobj[i] + " ");
+        }
 
+        // ArrayList Example
+        ArrayList<String> arrlistobj = new ArrayList<String>();
+        arrlistobj.add("Alive is awesome");
+        arrlistobj.add("Love yourself");
+        System.out.println("Arraylist size is : " + arrlistobj.size());
+
+        Iterator<String> it = arrlistobj.iterator();
+        System.out.println("====== ArrayList object output : ======");
+        while(it.hasNext()) {
+            System.out.println(it.next() + " ");
+        }
+
+        //LinkedList Example
+        LinkedList<String> linklistobj = new LinkedList<String>();
+        linklistobj.add("Tony is awesome");
+        linklistobj.add("Tony loves me");
+        System.out.println("LinkedList size is : " + linklistobj.size());
+
+        Iterator<String> itlinkedlist = linklistobj.iterator();
+        System.out.println("====== LinkedList object output : ======");
+        while(itlinkedlist.hasNext()) {
+            System.out.println(itlinkedlist.next() + " ");
+        }
 	}
 
 }
